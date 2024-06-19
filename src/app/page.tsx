989fce7a95cd377f42c28d6228edc28d7f5f352f
luthersharp <sharptrafficagency@gmail.com>
2024-06-19 22:39:48 +0100

    earlyaccess

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r from-teal-100 to-white py-80 lg:p-64">
+    <section className="px-0 max-w-screen-xl pb-4 lg:px-4 ">
+                <div className="text-center space-y-4">
+                    <h1 className="text-slate-900 font-bold text-xl lg:text-5xl md:text-2xl">
+                        We're Updtaing the platfrom
+                         <br/><span className="text-teal-400"> You'll gain access again <br/>very soon 👀</span>
+                    </h1>
+                    
+                </div>
+            </section>
+            </div>
+  );
+}
